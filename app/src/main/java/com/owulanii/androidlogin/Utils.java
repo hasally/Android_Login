35389136d49820b2dd93fc457b982ab9ca5ebf9f
hasally <hasally@naver.com>
2018-09-04 23:12:56 +0900

    Initial commit

---
+++ app/src/main/java/com/owulanii/androidlogin/Utils.java
@@ -0,0 +1,8 @@
+package com.owulanii.androidlogin;
+
+
+public class Utils {
+    public static final String BASE_IP = "http://172.20.10.3/";
+    public static final String LOGIN_URL = BASE_IP + "AndroidLoginWithMysql/login.php";
+    public static final String REGISTER_URL = BASE_IP + "AndroidLoginWithMysql/register.php";
+}
